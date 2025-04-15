9e86170a34359669d2c18cd1a8ece72f68c3d088
DanielaVM <mia.vargas@utec.edu.pe>
2025-04-15 17:08:28 -0500

    Estructura inicial de calculadora

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+def main():
+    print("Calculadora en línea de comandos")
+    print("Operaciones: +, -, *, /")
+    print("Presione 'c' para borrar, Enter para calcular")
+
+if __name__ == '__main__':
+    main()
